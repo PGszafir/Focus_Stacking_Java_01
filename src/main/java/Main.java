9abe3a07b336ee3f7cb78de6adaaec2e06667751
PGszafir <pgszafir@gmail.com>
2020-06-07 01:14:34 +0300

    Napis z Okienkiem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import org.joda.time.DateTime;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args){
         DateTime time=new DateTime();
         System.out.println("Focus Stacking Java Program");
-
+        JOptionPane.showMessageDialog(null,"Focus Stacking Java Program");
     }
 }
