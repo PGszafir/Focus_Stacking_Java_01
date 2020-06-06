343fb6aa4ff446f0dc87fe538b12de8fe8392f96
PGszafir <pgszafir@gmail.com>
2020-06-07 00:14:46 +0300

    Przeładowanie projektu do nowego repozytorium.

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.joda.time.DateTime;
+
+public class Main {
+    public static void main(String[] args){
+        DateTime time=new DateTime();
+        System.out.println("fs_01");
+
+    }
+}
