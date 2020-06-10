5ed7b27c64edf530f77aa6d8457f8eb0ab375d42
PGszafir <pgszafir@gmail.com>
2020-06-11 00:12:26 +0300

    komentarz do doto jpg

--- src/main/java/PixelMap.java
+++ src/main/java/PixelMap.java
@@ -5,3 +5,6 @@ public class PixelMap {
 //Jacek Jak to widzisz to tutaj dodawaj swoje funkcjonalnsci z operacjami na pixelach patrz co zakomitował łukasz
 // tak zeby wszystko się łaczyło
 //Mój komentarz - Jacek
+
+
+//PixelMapJPG*n ->PixelMap_mapa fęstości ostrości-> pixem map finalny-> JPG
