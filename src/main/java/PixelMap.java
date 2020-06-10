2f2d0b241aae481077b6d507b52f4f983d6c929f
PGszafir <pgszafir@gmail.com>
2020-06-10 23:52:10 +0300

    komentarz do doto jpg

--- src/main/java/PixelMap.java
+++ src/main/java/PixelMap.java
@@ -1,5 +1,6 @@
 public class PixelMap {
-
+//poeracje na macierzach pixeli
+    //export do klasy FotoJpg
 }
 //Jacek Jak to widzisz to tutaj dodawaj swoje funkcjonalnsci z operacjami na pixelach patrz co zakomitował łukasz
 // tak zeby wszystko się łaczyło
