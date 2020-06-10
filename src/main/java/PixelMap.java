9cbd57115a8d772f60e995df54755df69320301e
JacekBar <45076408+JacekBar@users.noreply.github.com>
2020-06-10 22:37:37 +0200

    Update PixelMap.java

--- src/main/java/PixelMap.java
+++ src/main/java/PixelMap.java
@@ -3,3 +3,4 @@ public class PixelMap {
 }
 //Jacek Jak to widzisz to tutaj dodawaj swoje funkcjonalnsci z operacjami na pixelach patrz co zakomitował łukasz
 // tak zeby wszystko się łaczyło
+//Mój komentarz - Jacek
