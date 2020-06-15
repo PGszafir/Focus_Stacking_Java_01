5df69419aea7b787c8f209bdfd4645b33c3d8eaa
PGszafir <pgszafir@gmail.com>
2020-06-15 23:17:40 +0300

    main rozpisanie funkcjonalności

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,15 @@ public class Main {
     public static void main(String[] args){
         DateTime time=new DateTime();
         System.out.println("Focus Stacking Java Program");
+        //miejsce na pobranie danych o pliku do odczytu
+
+
+        //użycie funkcji wczytaj z klasy FotoJPG w pętli
+
+
         JOptionPane.showMessageDialog(null,"Focus Stacking Java Program");
+
+
+        //JOptionPane.showMessageDialog(null,"zapisano wynki działania progrzmu do pliku");
     }
 }
