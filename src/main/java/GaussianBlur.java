c45be471b2f3aa45eaabef3d8dec660fbcd9262e
JacekBar <45076408+JacekBar@users.noreply.github.com>
2020-06-18 19:04:40 +0200

    Konstruktor klasy GaussianBlur

--- src/main/java/GaussianBlur.java
+++ src/main/java/GaussianBlur.java
@@ -1,5 +1,26 @@
 class GaussianBlur {
-    public GaussianBlur(String[] args) {
-        
+    public int[][] image;
+    private static int[][] matrix = [[1,2,1],[2,4,2],[1,2,1]];
+    public GaussianBlur(int[][] input_image) {
+        x = input_image.length;
+        y = input_image[0].length;
+        for(int i=0; i<x; i++) {
+            for(int j=0; j<y; j++){
+                if(i>0 && j>0 && i<x-1 && j<y-1){
+                    int[][] blur_matrix = [[input_image[i-1][j-1],input_image[i-1][j],input_image[i-1][j+1]],
+                                            [input_image[i][j-1],input_image[i][j],input_image[i][j+1]],
+                                            [input_image[i+1][j-1],input_image[i][j],input_image[i+1][j+1]]];
+                    intbuff
+                    for(int k=0; k<3; k++){
+                        for(int l=0; l<3; l++){
+
+                        }
+                    }
+                }
+                else{
+                    //dodaj niezmieniony piksel
+                }
+            }
+        }
     }
 }
