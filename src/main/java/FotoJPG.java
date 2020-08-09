acafa6efa854f9b2ecd9d8866fac2283df088b51
lukasmix06 <34247465+lukasmix06@users.noreply.github.com>
2020-08-09 14:35:01 +0200

    Stworzenie i wyświetlenie 2-wymiarowej tablicy pikseli w formacie [A R G B]

--- src/main/java/FotoJPG.java
+++ src/main/java/FotoJPG.java
@@ -25,16 +25,33 @@ public class FotoJPG extends JPanel {
             System.err.println("Blad odczytu obrazka");
             e.printStackTrace();
         }
-        int[] pixelArray = new int[image.getWidth()*image.getHeight()];
+        int[] pixelArray = new int[image.getWidth() * image.getHeight()];
         //próba uzyskania tablicy pikseli
         image.getRGB(0, 0, image.getWidth(), image.getHeight(), pixelArray, 0, image.getWidth());
+        //uzyskana tablica to tablica pikseli w postaci unsigned integer formy 0xAARRGGBB
         System.out.println(Arrays.toString(pixelArray));
-        //jest to tablica pikseli w postaci unsigned integer formy 0xAARRGGBB
-        //tu trzeba zrobić fora, aby otrzymywać tablicę krotek wartości kolorów składających się na każdy piksel
-        //int b = (pixelArray)&0xFF; //niebieski
-        //int g = (pixelArray>>8)&0xFF; //zielony
-        //int r = (pixelArray>>16)&0xFF; //czerwony
-        //int a = (pixelArray>>24)&0xFF; //alfa (?)
+
+
+        //tablica pikseli reprezentowanych przez tablicę 4 wartości kolorów składowych
+        int[][][] pixelArrayARGB = new int[image.getWidth()][image.getHeight()][4];
+        for (int i = 0; i < image.getWidth() * image.getHeight(); i++) {
+            int x = i / image.getWidth(); //pozycja na osi poziomej
+            int y = i % image.getWidth(); //pozycja na osi pionowej
+            pixelArrayARGB[y][x][0] = (pixelArray[i] >> 24) & 0xFF; //a, alfa
+            pixelArrayARGB[y][x][1] = (pixelArray[i] >> 16) & 0xFF; //r, czerwony
+            pixelArrayARGB[y][x][2] = (pixelArray[i] >> 8) & 0xFF; //g, zielony
+            pixelArrayARGB[y][x][3] = (pixelArray[i]) & 0xFF; //b, niebieski
+        }
+        for(int i = 0; i < 50; i++) {
+            for(int j = 0; j < 50; j++) {
+                System.out.print("[");
+                for(int k = 0; k < 4; k++) {
+                    System.out.print(" "+pixelArrayARGB[i][j][k]);
+                }
+                System.out.print("], ");
+            }
+            System.out.println();
+        }
         Dimension dimension = new Dimension(image.getWidth(), image.getHeight());
         setPreferredSize(dimension);
     }
