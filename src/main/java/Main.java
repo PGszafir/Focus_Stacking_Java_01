e5fbf9f1935755f872320ca249c0280e98529f65
lukasmix06 <34247465+lukasmix06@users.noreply.github.com>
2020-08-15 21:48:51 +0200

    Ponowna zmiana lokalizacji plików źródłowych

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import org.joda.time.DateTime;
+
+import javax.swing.*;
+
+//uruchomcie sobie i zobaczcie co się dzieje powinno najpierw długo budowac a za drugim razem szybciej
+public class Main {
+    public static void main(String[] args){
+        DateTime time=new DateTime();
+        System.out.println("Focus Stacking Java Program");
+        JOptionPane.showMessageDialog(null,"Focus Stacking Java Program");
+        new FotoDecomposition("psiaki.jpg");
+    }
+}
---
+++ src/main/java/PixelMap.java
@@ -0,0 +1,12 @@
+public class PixelMap {
+
+    //mapowanie ostrości jako 2 macierz tej klasy
+//poeracje na macierzach pixeli
+    //export do klasy FotoJpg
+}
+//Jacek Jak to widzisz to tutaj dodawaj swoje funkcjonalnsci z operacjami na pixelach patrz co zakomitował łukasz
+// tak zeby wszystko się łaczyło
+//Mój komentarz - Jacek
+
+
+//PixelMapJPG*n -> PixelMap_mapa fęstości ostrości-> pixem map finalny-> JPG
