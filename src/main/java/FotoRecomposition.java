a8fd71f53e6e64bc507fba766552a090d6d19d1a
lukasmix06 <34247465+lukasmix06@users.noreply.github.com>
2020-08-23 13:35:47 +0200

    Poprawki błędów, zapis mapy pikseli obrazu do pliku jpg, póki co nie działa dobrze

--- src/main/java/FotoRecomposition.java
+++ src/main/java/FotoRecomposition.java
@@ -10,19 +10,29 @@ public class FotoRecomposition {
     public FotoRecomposition(int[][][] inputImage) {
         int height = inputImage.length;
         int width = inputImage[0].length;
-        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        //Szablon na którym zmieniamy piksele
+        File initialImage = new File("resources/foto1.jpg");
+        try {
+            //utwórz obiekt z wczytanego zdjęcia
+            image = ImageIO.read(initialImage);
+        } catch (IOException e) {
+            System.err.println("Blad odczytu obrazka");
+            e.printStackTrace();
+        }
+
+        //image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB); TO NIE DZIAŁA TAK JAK POWINNO
         int[][] oneDimensionalArray = new int[height*width][4];
         for(int i = 0; i < height; i++){
             for(int j = 0; j < width; j++) {
-                oneDimensionalArray[height*i + j][0] = inputImage[i][j][0];
-                oneDimensionalArray[height*i + j][1] = inputImage[i][j][1];
-                oneDimensionalArray[height*i + j][2] = inputImage[i][j][2];
-                oneDimensionalArray[height*i + j][3] = inputImage[i][j][3];
+                oneDimensionalArray[width*i + j][0] = inputImage[i][j][0];
+                oneDimensionalArray[width*i + j][1] = inputImage[i][j][1];
+                oneDimensionalArray[width*i + j][2] = inputImage[i][j][2];
+                oneDimensionalArray[width*i + j][3] = inputImage[i][j][3];
             }
         }
 
-        int[] finalArray = new int[height*width];
-        for(int i = 0; i < width*height; i++) {
+        int[] finalArray = new int[height * width];
+        for(int i = 0; i < 100 ; i++) {
             finalArray[i] = (oneDimensionalArray[i][0] << 24) + (oneDimensionalArray[i][1] << 16) +
                     (oneDimensionalArray[i][2] << 8) + oneDimensionalArray[i][3];
         }
@@ -32,7 +42,7 @@ public class FotoRecomposition {
         File outputFile = new File("resources/stacked_foto.jpg");
 
         try {
-            ImageIO.write(image, "JPEG", outputFile);
+            ImageIO.write(image, "jpg", outputFile);
         } catch (IOException e) {
             System.err.println("Blad zapisu obrazka");
             e.printStackTrace();
