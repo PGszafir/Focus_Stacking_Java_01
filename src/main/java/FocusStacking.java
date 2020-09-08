c3dfe1a6b4088c07424a53cf1668fe4f6ec142b0
lukasmix06 <34247465+lukasmix06@users.noreply.github.com>
2020-09-08 18:38:49 +0200

    Wprowadzenie wykrywania dużej różnicy kolorów między wymienianymi pikselami w klasie FocusStacking w celu wyeliminowania przesunięcia

--- src/main/java/FocusStacking.java
+++ src/main/java/FocusStacking.java
@@ -1,34 +1,30 @@
+import static java.lang.Math.*;
+
 public class FocusStacking {
     private int [][] image;
     private short [][] focus;
     public FocusStacking(short[][] currentFocus, short[][] newFocus, int[][] currentImage, int[][] newImage){
         int height, width;
-        int COMPLEX_SIZE = 1;
+        int COLOR_TRESHOLD = 100;
         height = currentImage.length;
         width = currentImage[0].length;
         this.image = new int[height][width];
         this.focus = new short[height][width];
-        short [][] currentFocusComplex = new short[(height/COMPLEX_SIZE)+1][(width/COMPLEX_SIZE)+1];
-        short [][] newFocusComplex = new short[(height/COMPLEX_SIZE)+1][(width/COMPLEX_SIZE)+1];
-        for(int i=0; i < (height/COMPLEX_SIZE)+1; i++) {
-            for (int j = 0; j < (width/COMPLEX_SIZE)+1; j++) {
-                currentFocusComplex[i][j] = 0;
-                newFocusComplex[i][j] = 0;
-            }
-        }
-        for(int i=0; i<height; i++) {
-            for (int j = 0; j < width; j++) {
-                currentFocusComplex[i/COMPLEX_SIZE][j/COMPLEX_SIZE] += currentFocus[i][j];
-                newFocusComplex[i/COMPLEX_SIZE][j/COMPLEX_SIZE] += newFocus[i][j];
-            }
-        }
-
+        int colorDifference;
         for(int i=0; i<height; i++){
             for(int j=0; j<width; j++){
-                //if (newFocus[i][j] > currentFocus[i][j]){
-                if (newFocusComplex[i/COMPLEX_SIZE][j/COMPLEX_SIZE] > currentFocusComplex[i/COMPLEX_SIZE][j/COMPLEX_SIZE]) {
-                    this.image[i][j] = newImage[i][j];
-                    this.focus[i][j] = newFocus[i][j];
+                if (newFocus[i][j] > currentFocus[i][j]) {
+                    colorDifference = abs(getRed(currentImage[i][j]) - getRed(newImage[i][j])) +
+                            abs(getGreen(currentImage[i][j]) - getGreen(newImage[i][j])) +
+                            abs(getBlue(currentImage[i][j]) - getBlue(newImage[i][j]));
+                    if(colorDifference < COLOR_TRESHOLD){
+                        this.image[i][j] = newImage[i][j];
+                        this.focus[i][j] = newFocus[i][j];
+                    }
+                    else{
+                        this.image[i][j] = currentImage[i][j];
+                        this.focus[i][j] = currentFocus[i][j];
+                    }
                 }
                 else {
                     this.image[i][j] = currentImage[i][j];
@@ -46,5 +42,15 @@ public class FocusStacking {
     }
     public short[][] getFocus(){
         return this.focus;
+    }
+    public short getRed(int pixel) {
+        return (short)((pixel >> 16) & 0xFF);
+    }
+    public short getGreen(int pixel) {
+        return (short)((pixel >> 8) & 0xFF);
+
+    }
+    public short getBlue(int pixel) {
+        return (short)(pixel & 0xFF);
     }
 }
